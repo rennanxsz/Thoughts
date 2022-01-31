874dc31302c6f11da054d9da174a12928db43d81
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-01-30 21:22:26 -0300

    Creating a new project
    Iniciando mais um projeto do erik

---
+++ Thoughts/Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Rennan Bruno on 30/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
